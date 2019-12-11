7896e881c1b1c7e8f6707ac87531a01f4e2b1db9
AndrewBrown <volkovandrewig@yandex.ru>
2019-12-11 17:52:16 +0300

    Updates

---
+++ precache-manifest.21324f372114d36b4e5f797a38d2e378.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db9fc3369a6fde0d157cebf8feb97cf8",
+    "url": "/TrueGladiator/index.html"
+  },
+  {
+    "revision": "e2de2ced42b35d85b502",
+    "url": "/TrueGladiator/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "a5e7e7a204bf1f7fafc0",
+    "url": "/TrueGladiator/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "e2de2ced42b35d85b502",
+    "url": "/TrueGladiator/static/js/2.cba1b5e2.chunk.js"
+  },
+  {
+    "revision": "a5e7e7a204bf1f7fafc0",
+    "url": "/TrueGladiator/static/js/main.c7a4252e.chunk.js"
+  },
+  {
+    "revision": "79db8916cfcdc87ce242",
+    "url": "/TrueGladiator/static/js/runtime-main.fe2977c2.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/TrueGladiator/static/media/persik.4e1ec840.png"
+  }
+]);
