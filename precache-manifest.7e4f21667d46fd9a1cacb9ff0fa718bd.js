70d562b9a90b2e0a9cca7e6891476446b4a85bd2
AndrewBrown <volkovandrewig@yandex.ru>
2019-12-11 19:08:21 +0300

    Updates

--- precache-manifest.7e4f21667d46fd9a1cacb9ff0fa718bd.js
+++ precache-manifest.7e4f21667d46fd9a1cacb9ff0fa718bd.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "48b6a9713e6869d1af58beec29332327",
+    "revision": "4cd91b206715f42ab31f081c4c8b85ad",
     "url": "/TrueGladiator/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/TrueGladiator/static/css/2.4e89ce74.chunk.css"
   },
   {
-    "revision": "d52be9218a749f65a852",
+    "revision": "a3870c4f67159f25ab78",
     "url": "/TrueGladiator/static/css/main.981f7211.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/TrueGladiator/static/js/2.d166d34d.chunk.js"
   },
   {
-    "revision": "d52be9218a749f65a852",
-    "url": "/TrueGladiator/static/js/main.9c72fc2c.chunk.js"
+    "revision": "a3870c4f67159f25ab78",
+    "url": "/TrueGladiator/static/js/main.2a2e6d8f.chunk.js"
   },
   {
     "revision": "79db8916cfcdc87ce242",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TrueGladiator/precache-manifest.cfde83522df1a6ee7592b29b84d5a822.js"
+  "/TrueGladiator/precache-manifest.7e4f21667d46fd9a1cacb9ff0fa718bd.js"
 );
 
 self.addEventListener('message', (event) => {
